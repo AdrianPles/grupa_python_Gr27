6ea8f1a41f3cb4ebc5796c5c9fedd20d1f3e71a5
AdrianPles <adi_kite@yahoo.com>
2026-06-25 20:58:46 +0300

    classes level 2

--- 15_classes_2.py
+++ 15_classes_2.py
@@ -34,9 +34,10 @@ class Task:
         self.date = date
         self.owner = owner
         self.category = category
+        self.completed = False
 
     def __str__(self):
-        return f"{self.title}, {self.date}, {self.owner}, {self.category}"
+        return f"{self.title}, {self.date}, {self.owner}, {self.category}, completed = {self.completed}"
 
     def __repr__(self):
         return f'Task("{self.title}", "{self.date}", "{self.owner}", {self.category})'
@@ -50,22 +51,49 @@ task3 = Task("Buy shoes", "24.Iunie", "Olivia", Categories.SHOPPING)
 
 # todo = [task1, task2, task3]
 
+
 class Todos:
     def __init__(self):
         self.todos_list = []
 
+    # property, this is just like a class attribute, that gets calculated whenever it's read.
+    # # if a programmer writes todos1.task_count, the value gets calculated on the spot, every time this property is read.
+    # @property
+    # def task_count(self):
+    # return len(self.todos_list)
+
+    @property
+    def task_count(self):
+        return len(self.todos_list)
+
 # todo this
     def add_task(self, task):
-        for elem in self.todos_list:
-            if task == elem:
-                print("Task with this title already exists!")
-            else:
-                self.todos_list.append(task)
+        self.todos_list.append(task)
+        # for elem in self.todos_list:
+        #     if self.title == elem:
+        #         print("Task with this title already exists!")
+        #     else:
+        #         self.todos_list.append(task)
+        # return self.todos_list
 
     def remove_task(self, task_to_delete):
         for task in self.todos_list:
             if task.title == task_to_delete.title:
                 self.todos_list.remove(task)
+
+    def mark_as_completed(self, task: Task):
+        task.completed = True
+
+    def filter_by_completed(self, is_completed: bool):
+        # aceasta functie sa returneze toate task-urile din todos_list,
+        # care sunt completed sau nu, in functie de argumentul primit.
+        lista = []
+        for t in self.todos_list:
+            if t.completed == is_completed:
+                lista.append(t)
+        return lista
+
+
 
     def filter_by_category(self, categ):
         results = []
@@ -101,25 +129,52 @@ todos1.add_task(task2)
 todos1.add_task(task3)
 todos1.add_task(Task("Go to second-hand store", "25.June", "Olivia", Categories.SHOPPING))
 
-todos1.remove_task(task2)
-print(todos1)
-
-print(todos1.filter_by_category(Categories.SHOPPING))
 print(task1)
+todos1.mark_as_completed(task1)
+print(task1)
+
+
+print(" =========== Task filtered by completed:")
+print(todos1.filter_by_completed(False))
+
+
+print("==============================")
+
+
+todos1.add_task(("Write a poem", "Today", "olivia", Categories.PRESENTS))
+
+print("===========Task count ================")
+print(len(todos1.todos_list))
+print((todos1.task_count))
+
+print("================================")
+
+# print(todos1)
+# print("\n")
+# print('-' * 15, 'Stergere task', '-' * 15)
+# todos1.remove_task(task2)
+# print(todos1)
+#
+# print('\n','-' * 15, 'Filtrare dupa categorie', '-' * 15)
+# print(todos1.filter_by_category(Categories.SHOPPING))
+
+
 
 task4 = Task("name", "23.June", "owner", Categories.SHOPPING)
 
-# Categories.SHOPPING     /// "shoping"
+# Categories.SHOPPING     ///       "shopping"
 
 task5 = Task("Rezolvare Tema", "23.IUNIE", "John", Categories.COURSE)
 print(task5.category)
 
-task6 = Task("Rezolvare Tema", "23.IUNIE", "Oliver", Categories.SHOPPING)
-todos1.add_task(task6)
-
-
-
-print(todos1.filter_by_owner("John"))
+# print('\n','-' * 15, 'Tipariere lista totala', '-' * 15)
+# print(todos1)
+# task6 = Task("Rezolvare Tema", "23.IUNIE", "Oliver", Categories.COURSE)
+# todos1.add_task(task6)
+# print(todos1)
+#
+# print('\n','-' * 15, 'Filtrare lista owner', '-' * 15)
+# print(todos1.filter_by_owner("Olivia"))
 
 # scrieti o metoda in clasa Todos pentru a filtra dupa owner.
 # acea metoda va returna toate task-urile ale unui owner, ce-l primim ca parametru al
@@ -133,3 +188,6 @@ print(todos1.filter_by_owner("John"))
 # modificati metoda add_task, sa nu permita adaugarea unui task cu titlu duplicat.
 # Daca exista deja un task cu acel titlu, sa printeze "Task with this title already
 # exists!.
+
+for c in Categories:
+    print(c)
